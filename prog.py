54cd1ae4e3b02766a69b61547a6e875294e31d7a
PetraBattistata <127765772+PetraBattistata@users.noreply.github.com>
2023-03-13 16:25:11 +0100

    Update prog.py

--- prog.py
+++ prog.py
@@ -1,4 +1,4 @@
 # ! python3
 import streamlit as st
-st.header("hello world")  
-st.text("from Brixen")
+title = st.text_input('Please insert a movie title', '')
+st.write('The current movie title is', title)
